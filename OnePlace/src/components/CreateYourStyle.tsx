06ea6a2021cd1ffd88a567b9b0a6acc093815483
Sara sofia grijalba <grijalbapsaras.cla@gmail.com>
2025-10-16 16:11:28 -0500

    file: change photos to img file

--- OnePlace/src/components/CreateYourStyle.tsx
+++ OnePlace/src/components/CreateYourStyle.tsx
@@ -4,7 +4,7 @@ const CreateYourStyle = () => {
     <div
       className="relative w-full h-[600px] rounded-3xl overflow-hidden bg-center bg-cover" // 👈 agrega tu ruta de imagen aquí
     >
-        <img src="./img/CreateYourStyle.svg" alt="Seccion create your style" />
+        <img src="./img/collageimag.svg" alt="Seccion create your style" />
       <div className="absolute inset-0 bg-black/20"></div>
 
       <div className="absolute right-10 top-1/2 -translate-y-1/2 text-right max-w-md">
