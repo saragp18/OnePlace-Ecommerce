c816aea6b0dd680acb4865a77192f63cc382a890
Sara sofia grijalba <grijalbapsaras.cla@gmail.com>
2025-10-16 21:07:00 -0500

    feat: section NewItems

--- OnePlace/src/App.tsx
+++ OnePlace/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Home from './pages/home'
 import CreateYourStyle from './components/CreateYourStyle'
+import NewItems from './components/NewItems'
 function App() {
 
   return (
     <>
       < Home />
+      <NewItems/>
       <CreateYourStyle />
 
     </>
