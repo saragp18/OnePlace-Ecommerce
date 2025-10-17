cf833129b1893d4739d32cdd8d1e859e79d4593d
Sara sofia grijalba <grijalbapsaras.cla@gmail.com>
2025-10-17 10:00:58 -0500

    feat: section App

--- OnePlace/src/App.tsx
+++ OnePlace/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Home from './pages/home'
+import Home from './components/Hero'
 import CollageVass from './components/NewItems'
 import Categories from "./components/Categories"
 import CreateYourStyle from './components/CreateYourStyle'
