b164fb8899d4b1b79bc57de26f9bafa73133e948
Sara sofia grijalba <grijalbapsaras.cla@gmail.com>
2025-10-17 10:03:02 -0500

    fix: bad naming

--- OnePlace/src/pages/home.tsx
+++ OnePlace/src/pages/home.tsx
@@ -1,38 +1,24 @@
-
-import SearchBar from "../components/Navbar";
-import ViewAllBtn from "../components/ViewAllBtn";
-import sweaterImg from "/img/sueter one-place.jpg";
+import Categories from '../components/Categories'
+import CreateYourStyle from '../components/CreateYourStyle'
+import Footer from '../components/Footer'
+import Hero from '../components/Hero'
+import CollageVass from '../components/NewItems'
 
 function Home() {
+
   return (
-    <div className="relative w-full min-h-screen flex flex-col bg-gray-50">
-     
-      <div className="absolute top-0 left-0 w-full z-20">
-        <SearchBar />
-      </div>
+    <>
+      <Hero />
+      <CollageVass/>
+      <Categories />
+      <CreateYourStyle />
+      <Footer/>
+    
+    
 
-     
-      <div className="w-full flex flex-col md:flex-row h-screen">
-        
-        <div
-          className="w-full md:w-1/2 h-64 sm:h-80 md:h-full bg-cover bg-center transition-all duration-500"
-          style={{ backgroundImage: `url(${sweaterImg})` }}
-        ></div>
 
-        
-        <div className="w-full md:w-1/2 flex flex-col items-center md:items- justify-center sm:px-8 md:px-16 py-8 sm:py-12 text-center md:text-left bg-white transition-all duration-300">
-          <h1 className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-ligt text-black leading-tight">
-            WINTER
-          </h1>
-          <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-semibold text-black mb-8 sm:mb-10 tracking-wide">
-            COLLECTION
-          </h1>
-
-          <ViewAllBtn />
-        </div>
-      </div>
-    </div>
-  );
+    </>
+  )
 }
 
-export default Home;
+export default Home
