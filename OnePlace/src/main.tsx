6622929c89749a53ffc4ba77ce678000c45662fb
Sara sofia grijalba <grijalbapsaras.cla@gmail.com>
2025-10-17 10:03:31 -0500

    feat: react router added

--- OnePlace/src/main.tsx
+++ OnePlace/src/main.tsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/home.tsx'
+const routes= createBrowserRouter ([{
+  path:"/home",
+  element:<Home/>
+
+},
+{
+
+}])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={routes}/>
   </StrictMode>,
 )
