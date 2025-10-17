821d865e30bd0ca676f0d6d0283c29f961f97106
Sara sofia grijalba <grijalbapsaras.cla@gmail.com>
2025-10-16 23:51:31 -0500

    feat: section home

--- OnePlace/src/pages/home.tsx
+++ OnePlace/src/pages/home.tsx
@@ -1,4 +1,4 @@
-/*import React from "react";*/
+
 import SearchBar from "../components/Navbar";
 import ViewAllBtn from "../components/ViewAllBtn";
 import sweaterImg from "/img/sueter one-place.jpg";
