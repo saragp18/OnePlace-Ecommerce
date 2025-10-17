84f8c2a98c4ef6999f50b134168b6da95f44b336
Sara sofia grijalba <grijalbapsaras.cla@gmail.com>
2025-10-17 01:16:58 -0500

    feat: section App

--- OnePlace/src/App.tsx
+++ OnePlace/src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Home from './pages/home'
-import NewItems from './components/NewItems'
+import CollageVass from './components/NewItems'
 import Categories from "./components/Categories"
 import CreateYourStyle from './components/CreateYourStyle'
 import Footer from './components/Footer'
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       < Home />
-      <NewItems/>
+      <CollageVass/>
       <Categories />
       <CreateYourStyle />
       <Footer/>
