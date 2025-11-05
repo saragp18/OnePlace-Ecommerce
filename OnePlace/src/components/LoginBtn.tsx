123de14fa6f526680ea079dc152a5ac35aaf2f7f
Alejandro Acosta <alejo.acosta.q@gmail.com>
2025-11-04 21:45:32 -0500

    file: components and page file created

---
+++ OnePlace/src/components/LoginBtn.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Login() {
+  return (
+    <button className="px-8 py-3 border border-gray-300 rounded-full bg-white text-black font-semibold text-sm hover:bg-gray-800 hover:text-white transition-all duration-300">
+      Log In
+    </button>
+  );
+}
+
+export default Login;
---
+++ OnePlace/src/components/confirmPassword.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+
+function ContrasenaConfirma() {
+  return (
+    <>
+    
+    <input type='text' name='Password' placeholder="Password"  className="w-full max-w-sm px-4 py-2 bg-gray-200 rounded-xl border-none focus:outline-none shadow-inner"/>
+    </>
+            
+      
+  );
+}
+
+export default ContrasenaConfirma;
