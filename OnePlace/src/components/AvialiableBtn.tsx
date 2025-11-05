a348a16f7db69c5756ae822d4cc81c64ca7b898d
Alejandro Acosta <alejo.acosta.q@gmail.com>
2025-11-04 22:41:28 -0500

    feature: add porduct form created

---
+++ OnePlace/src/components/AvialiableBtn.tsx
@@ -0,0 +1,10 @@
+function Available() {
+  return (
+    <button className="px-8 py-3 border border-gray-300 rounded-full bg-white text-black font-semibold text-sm hover:bg-gray-800 hover:text-white transition-all duration-300">
+      Available
+    </button>
+    
+  );
+}
+
+export default Available;
---
+++ OnePlace/src/components/CategoryMenu.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+function CategoryMenu() {
+  return (
+    <div className="bg-gray-100 p-4 rounded-xl grid grid-cols-2 gap-2">
+      <button className="px-4 py-2 rounded-full bg-white text-gray-800 font-medium text-sm shadow-sm hover:bg-gray-900 hover:text-white transition-all duration-300">
+        Women
+      </button>
+
+      <button className="px-4 py-2 rounded-full bg-white text-gray-800 font-medium text-sm shadow-sm hover:bg-gray-900 hover:text-white transition-all duration-300">
+        Men
+      </button>
+
+      <button className="px-4 py-2 rounded-full bg-white text-gray-800 font-medium text-sm shadow-sm hover:bg-gray-900 hover:text-white transition-all duration-300">
+        Jackets
+      </button>
+
+      <button className="px-4 py-2 rounded-full bg-white text-gray-800 font-medium text-sm shadow-sm hover:bg-gray-900 hover:text-white transition-all duration-300">
+        Jeans
+      </button>
+
+      <button className="px-4 py-2 rounded-full bg-white text-gray-800 font-medium text-sm shadow-sm hover:bg-gray-900 hover:text-white transition-all duration-300">
+        Shirts
+      </button>
+
+      <button className="px-4 py-2 rounded-full bg-white text-gray-800 font-medium text-sm shadow-sm hover:bg-gray-900 hover:text-white transition-all duration-300">
+        T-Shirts
+      </button>
+
+      <button className="px-4 py-2 rounded-full bg-white text-gray-800 font-medium text-sm shadow-sm hover:bg-gray-900 hover:text-white transition-all duration-300">
+        Bermuda Shorts
+      </button>
+
+      <button className="px-4 py-2 rounded-full bg-white text-gray-800 font-medium text-sm shadow-sm hover:bg-gray-900 hover:text-white transition-all duration-300">
+        Sweaters
+      </button>
+    </div>
+  );
+}
+
+export default CategoryMenu;
+
+
