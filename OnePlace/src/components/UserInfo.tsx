0bc074300ff20ba0341f22e7b1c575e7f8b944e2
Alejandro Acosta <alejo.acosta.q@gmail.com>
2025-11-27 14:34:54 -0500

    feat: user archives created

---
+++ OnePlace/src/components/UserInfo.tsx
@@ -0,0 +1,34 @@
+/*import React from "react";
+import { useSelector } from "react-redux";
+import LogOut from "./LogOutBtn";
+
+function UserInfo() {
+  const user = useSelector((state) => state.user.data);
+
+  return (
+    <div
+      className="
+          w-full md:w-fit 
+          bg-gray-200 
+          p-6 md:p-10 
+          flex flex-col 
+          md:min-h-full
+        "
+    >
+      <h1 className="text-2xl font-semibold text-gray-800">
+        {user?.name || "Cargando..."}
+      </h1>
+
+      <div className="mt-4 space-y-1 text-gray-700">
+        <h2>{user?.phone}</h2>
+        <h2>{user?.email}</h2>
+      </div>
+
+      <div className="mt-8">
+        <LogOut />
+      </div>
+    </div>
+  );
+}
+
+export default UserInfo;*/
---
+++ OnePlace/src/pages/UserPage.tsx
@@ -0,0 +1,29 @@
+/*import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../redux/store";
+import { fetchUser } from "../redux/userSlice";
+import Navbar from "../components/SearchBar";
+import UserInfo from "../components/UserInfo";
+
+const UserPage: React.FC = () => {
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    const userId = localStorage.getItem("userId");
+    if (userId) dispatch(fetchUser(userId));
+  }, [dispatch]);
+
+  return (
+    <div className="min-h-screen bg-gray-50 flex flex-col">
+      <Navbar />
+
+      <div className="flex flex-col md:flex-row flex-1">
+        <UserInfo />
+
+        <div className="flex-1 p-6 md:p-10"></div>
+      </div>
+    </div>
+  );
+};
+
+export default UserPage;*/
