4abac39f75e47e6d2d244f7627327230779af468
Alejandro Acosta <alejo.acosta.q@gmail.com>
2025-11-28 16:20:16 -0500

    feature: publication dependencies put

--- OnePlace/vite.config.ts
+++ OnePlace/vite.config.ts
@@ -5,4 +5,20 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+
+  preview: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT || '4173'),
+    strictPort: true,
+    allowedHosts: [
+      'OnePlace-losNentindos.onrender.com',
+      '.onrender.com', // Permite todos los subdominios de Render
+      'localhost',
+    ],
+  },
+  server: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT || '5173'),
+    strictPort: true,
+  },
 })
